050c5b4f1feaecde6c75f023798bde431c238bc7
JoséCarlos8002 <juninhooliveira1@hotmail.com>
2026-01-12 12:09:49 -0300

    Cleanup: Remove debug logs

--- index.tsx
+++ index.tsx
@@ -7,7 +7,6 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-console.log("index.tsx: Root element found, preparing to mount React app...");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
